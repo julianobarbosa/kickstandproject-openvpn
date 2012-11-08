598b22bb2bcddf7f2d93ee5df3a94622640c394a
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-08 10:38:46 -0500

    Add openvpn::function::tunnell::server
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ manifests/function/tunnel/server.pp
@@ -0,0 +1,42 @@
+# == Define: openvpn::function::tunnel::server
+#
+# === Parameters
+#
+# === Variables
+#
+# === Examples
+#
+# === Authors
+#
+# Paul Belanger <paul.belanger@polybeacon.com>
+#
+# === Copyright
+#
+# Copyright (C) 2012, PolyBeacon, Inc.
+#
+# This program is free software, distributed under the terms
+# of the Apache License, Version 2.0. See the LICENSE file at
+# the top of the source tree.
+#
+class openvpn::function::tunnel::server(
+  $server,
+  $port = '1194',
+  $proto = 'udp',
+  $dev = 'tun',
+  $cert = "${name}.crt",
+  $key = "${name}.key",
+  $dh = '2048',
+  $keepalive = '10 120',
+  $tls-auth = 'ta.key',
+  $cipher = 'BF-CBC',
+  $compression = true,
+) {
+  file { "${openvpn::params::basedir}/${name}.conf":
+    ensure  => file,
+    content => template('openvpn/server/etc/openvpn/server.conf.erb'),
+    notify  => Class['openvpn::server::service'],
+    require => File[$openvpn::params::basedir],
+  }
+}
+
+# vim:sw=2:ts=2:expandtab:textwidth=79
